786141d405fe3f87641d4650be59c57bfa8c5bcd
LobsterLobsterski <100423134+LobsterLobsterski@users.noreply.github.com>
2022-06-01 15:54:28 +0200

    Add files via upload
    boolean finished(String maybe)

---
+++ projektZaliczeniowy1/projektZaliczeniowy/src/com/company/Szef.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class Szef extends Pracownik{
+    protected String login="admin";
+    protected String haslo="admin";
+
+}
---
+++ projektZaliczeniowy1/projektZaliczeniowy/src/com/company/Zamowienie.java
@@ -0,0 +1,100 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Zamowienie {
+    int nr;
+    int ilosc;
+    Kucharz.rodzajeKebabow rodzaj;
+    float cena;
+    String nazwisko;
+    Kucharz kucharz;
+
+    int czasWykonania;
+    private Scanner scanner = new Scanner(System.in);
+
+    Zamowienie(){
+        System.out.println("Rodzaj kebaba: \n"+"1. rollo \n"+"2 bułka \n"+"3 talerz \n" +"4 box \n");
+        int poj=scanner.nextInt();
+        switch (poj){
+            case 1:
+                rodzaj= Kucharz.rodzajeKebabow.rollo;
+                break;
+            case 2:
+                rodzaj= Kucharz.rodzajeKebabow.bulka;
+                break;
+            case 3:
+                rodzaj= Kucharz.rodzajeKebabow.talerz;
+                break;
+            default:
+                rodzaj= Kucharz.rodzajeKebabow.box;
+        }
+        System.out.println("Ile?");
+        ilosc=scanner.nextInt();
+        System.out.println("Cena: ");
+        cena=scanner.nextFloat();
+        System.out.println("Nazwisko kucharza: ");
+        nazwisko=scanner.next();
+
+    }
+
+    Zamowienie(int nr, ArrayList<Pracownik> konta){
+        this();
+        this.nr=nr;
+        setKucharz(nazwisko, konta);
+        czasWykonania=(int)kucharz.czasRobieniaKebaba(rodzaj, ilosc);
+
+    }
+
+    Zamowienie(int nr, int ilosc, float cena, String nazwisko, ArrayList<Pracownik> konta){
+        this.nr=nr;
+        this.ilosc=ilosc;
+        this.cena=cena;
+        this.nazwisko=nazwisko;
+
+        setKucharz(nazwisko, konta);
+        czasWykonania=(int)kucharz.czasRobieniaKebaba(rodzaj, ilosc);
+    }
+
+    public void wyswietl(){
+        System.out.print("nr zamówienia: "+nr+"\n");
+        System.out.println("\tRodzaj kebaba: "+rodzaj);
+        System.out.println("\tIlość: "+ilosc);
+        System.out.println("\tCena: "+cena);
+        System.out.println("\tKucharz: "+kucharz.imie+" "+kucharz.nazwisko);
+        System.out.println("\tCzasWykonania: "+czasWykonania+" min");
+        System.out.println();
+    }
+
+    public void setKucharz(String nazwisko, ArrayList<Pracownik> konta) {
+
+        boolean jest=false;
+        while (jest==false) {
+
+            for (Pracownik p : konta) {
+                if (p instanceof Kucharz && p.nazwisko.equals(nazwisko)) {
+                    kucharz = (Kucharz) p;
+                    jest = true;
+                    break;
+                }
+            }
+            if (jest == false) {
+                System.out.println("Nieznaleziono kucharza o nazwisku "+nazwisko+"\nSpróbuj ponownie.");
+                for (Pracownik p : konta) {
+                    if (p instanceof Kucharz) {
+                        System.out.println("ID: " + p.id + "\n\t" + p.imie + " " + p.nazwisko);
+                    }
+                }
+                nazwisko=scanner.next();
+                this.nazwisko=nazwisko;
+            }
+
+        }
+    }
+
+    public int getCzasWykonania() {
+        return czasWykonania;
+    }
+}
